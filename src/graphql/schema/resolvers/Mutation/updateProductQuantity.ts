a104aec567c38b12bd24083040f4cfb9076dde6e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2023-10-06 01:33:13 +0200

    feat(cart): add product quantity update

--- src/graphql/schema/resolvers/Mutation/updateProductQuantity.ts
+++ src/graphql/schema/resolvers/Mutation/updateProductQuantity.ts
@@ -1,6 +1,16 @@
 import type { MutationResolvers } from "./../../../types.generated";
 export const updateProductQuantity: NonNullable<
 	MutationResolvers["updateProductQuantity"]
-> = async (_parent, _arg, _ctx) => {
-	/* Implement Mutation.updateProductQuantity resolver logic here */
+> = async (_parent, arg, ctx) => {
+	const orderItem = await ctx.prisma.orderItem.update({
+		where: {
+			id: arg.productId,
+		},
+		data: {
+			quantity: arg.quantity,
+			total: arg.total,
+		},
+	});
+
+	return { ...orderItem };
 };
--- src/graphql/schema/resolvers/Mutation/updatedProductQuantity.ts
+++
@@ -1,6 +0,0 @@
-import type { MutationResolvers } from "./../../../types.generated";
-export const updatedProductQuantity: NonNullable<
-	MutationResolvers["updatedProductQuantity"]
-> = async (_parent, _arg, _ctx) => {
-	/* Implement Mutation.updatedProductQuantity resolver logic here */
-};
